2459a24f3b8a7d85784634247ca10d0de36b56d6
Truong Phuoc <hoangphuoc25@gmail.com>
2014-04-17 06:15:33 +0800

    done

--- tryRequests.py
+++ tryRequests.py
@@ -1,46 +1,67 @@
 #!python3
-
+import os
 import requests
 from html.parser import HTMLParser
 from urllib import parse
 
 class LinkParser(HTMLParser):
-	def __init__(self):
+	def __init__(self, url):
 		HTMLParser.__init__(self)
 		self.links = []
+		self.baseUrl = url
+		self.htmlLinks = [url]
 
 	def handle_starttag(self, tag, attrs):
 		if tag == 'a':
+			# print(attrs)
 			for (key, value) in attrs:
 				if key == 'href':
 					newUrl = parse.urljoin(self.baseUrl, value)
 					if newUrl in self.baseUrl or '?' in value:
+						# print("DISCARDED: ",newUrl, "BECAUSE OF: ", self.baseUrl)
 						continue
 					testNewUrl = requests.get(newUrl)
-					if testNewUrl.status_code != 404 and \
-							testNewUrl.headers['content-type'].split(";")[0] == 'application/pdf':
-						self.links = self.links + [newUrl]
+					# print(newUrl)
+					if testNewUrl.status_code == 200 and \
+							testNewUrl.headers['content-type'].split(";")[0] != 'text/html':
+						# print("PDF:    ",newUrl)
+						self.links.append(newUrl)
 					elif testNewUrl.status_code != 404 and \
 							testNewUrl.headers['content-type'].split(";")[0] == 'text/html':
-						print ("HTML: ",newUrl)
-						self.getLink(newUrl)
-	def getLink(self, url):
-		self.baseUrl = url
-		# while self.htmlLinks != []:
-		# 	visiting = self.htmlLinks[0]
-		# 	self.htmlLinks = self.htmlLinks[1:]
-		visiting = url
-		response = requests.get(visiting)
-		if response.headers['content-type'].split(";")[0] == 'text/html':
-			htmlString = response.content.decode("utf-8")
-			self.feed(htmlString)
-		
+						#print ("HTML: ",newUrl)
+						self.htmlLinks.append(newUrl)
+						#print(self.htmlLinks)
+	def getLink(self):
+		while self.htmlLinks != []:
+			visiting = self.htmlLinks[0]
+			print("VISITING: ",visiting)
+			self.htmlLinks = self.htmlLinks[1:]
+			response = requests.get(visiting)
+			if response.headers['content-type'].split(";")[0] == 'text/html':
+				# print("Link OK, feeding into HTMLParser")
+				htmlString = response.content.decode("utf-8")
+				self.baseUrl = visiting
+				self.feed(htmlString)
+		# print(self.links)
+
+def fileName(url):
+	split = url.split('/')[-1]
+	return split
+def download(parser):
+	for x in parser.links:
+		# filePath = os.path.join("C:/OSS/download", fileName(x))
+		filePath = "C:/OSS/download/" + fileName(x)
+		print(filePath)
+		f = open(filePath, 'wb')
+		r = requests.get(x)
+		f.write(r.content)
+		f.close()
 
 def tryRequests():
-	parser = LinkParser()
 	url = "http://clgiles.ist.psu.edu/IST441/materials/"
-	parser.getLink(url)
-	#download(parser)
+	parser = LinkParser(url)
+	parser.getLink()
+	download(parser)
 	#print(parser.links)
 
 if __name__ == "__main__":
