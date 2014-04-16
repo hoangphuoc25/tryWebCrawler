f85bc9501f56dff28599ea0fc0aad00becab0187
Truong Phuoc <hoangphuoc25@gmail.com>
2014-04-17 07:16:05 +0800

    some tweaks

--- tryRequests.py
+++ tryRequests.py
@@ -1,4 +1,5 @@
 #!python3
+import sys
 import os
 import requests
 from html.parser import HTMLParser
@@ -13,24 +14,18 @@ class LinkParser(HTMLParser):
 
 	def handle_starttag(self, tag, attrs):
 		if tag == 'a':
-			# print(attrs)
 			for (key, value) in attrs:
 				if key == 'href':
 					newUrl = parse.urljoin(self.baseUrl, value)
 					if newUrl in self.baseUrl or '?' in value:
-						# print("DISCARDED: ",newUrl, "BECAUSE OF: ", self.baseUrl)
 						continue
 					testNewUrl = requests.get(newUrl)
-					# print(newUrl)
 					if testNewUrl.status_code == 200 and \
 							testNewUrl.headers['content-type'].split(";")[0] != 'text/html':
-						# print("PDF:    ",newUrl)
 						self.links.append(newUrl)
 					elif testNewUrl.status_code != 404 and \
 							testNewUrl.headers['content-type'].split(";")[0] == 'text/html':
-						#print ("HTML: ",newUrl)
 						self.htmlLinks.append(newUrl)
-						#print(self.htmlLinks)
 	def getLink(self):
 		while self.htmlLinks != []:
 			visiting = self.htmlLinks[0]
@@ -38,31 +33,32 @@ class LinkParser(HTMLParser):
 			self.htmlLinks = self.htmlLinks[1:]
 			response = requests.get(visiting)
 			if response.headers['content-type'].split(";")[0] == 'text/html':
-				# print("Link OK, feeding into HTMLParser")
 				htmlString = response.content.decode("utf-8")
 				self.baseUrl = visiting
 				self.feed(htmlString)
-		# print(self.links)
-
 def fileName(url):
 	split = url.split('/')[-1]
 	return split
-def download(parser):
+def download(parser, destination):
 	for x in parser.links:
-		# filePath = os.path.join("C:/OSS/download", fileName(x))
-		filePath = "C:/OSS/download/" + fileName(x)
-		print(filePath)
+		# filePath = os.path.join(destination, fileName(x))
+		filePath = destination + fileName(x)
+		dir = os.path.dirname(filePath)
+		try:
+			os.stat(dir)
+		except:
+			os.mkdir(dir)
 		f = open(filePath, 'wb')
 		r = requests.get(x)
 		f.write(r.content)
 		f.close()
 
-def tryRequests():
-	url = "http://clgiles.ist.psu.edu/IST441/materials/"
+def Requests(url, destination):
 	parser = LinkParser(url)
 	parser.getLink()
-	download(parser)
-	#print(parser.links)
+	download(parser, destination)
 
 if __name__ == "__main__":
-	tryRequests()
+	if len(sys.argv) < 2:
+		print("Enter path to search for file and destination folder")
+	Requests(sys.argv[1], sys.argv[2])
